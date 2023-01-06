28ee257b059f0756bac78869bbfcd5db5e9b5554
Drlazinho <lazbonfim@hotmail.com>
2023-01-05 22:44:31 -0300

    performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,51 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react'
 
-import { SideBar } from './components/SideBar';
-import { Content } from './components/Content';
+import { SideBar } from './components/SideBar'
+import { Content } from './components/Content'
 
-import { api } from './services/api';
+import { api } from './services/api'
 
-import './styles/global.scss';
+import './styles/global.scss'
 
-import './styles/sidebar.scss';
-import './styles/content.scss';
-
+import './styles/sidebar.scss'
+import './styles/content.scss'
 
 interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
+  id: number
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+  title: string
 }
 
 export function App() {
-  const [selectedGenreId, setSelectedGenreId] = useState(1);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [selectedGenreId, setSelectedGenreId] = useState(1)
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  )
 
   useEffect(() => {
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`)
-    .then(response => {
-      setSelectedGenre(response.data);
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data)
     })
-  }, [selectedGenreId]);
+  }, [selectedGenreId])
 
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
+  // function handleClickButton(id: number) {
+  //   setSelectedGenreId(id);
+  // }
+
+  const handleClickButton = useCallback((id: number) => {
+    setSelectedGenreId(id)
+  }, [])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <SideBar selectedGenreId={selectedGenreId} handleClickButton={handleClickButton}/>
-      <Content selectedGenre={selectedGenre} selectedGenreId={selectedGenreId}/>     
+      <SideBar
+        selectedGenreId={selectedGenreId}
+        handleClickButton={handleClickButton}
+      />
+      <Content
+        selectedGenre={selectedGenre}
+        selectedGenreId={selectedGenreId}
+      />
     </div>
   )
 }
